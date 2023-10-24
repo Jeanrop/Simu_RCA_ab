4486db03d68f252be59dd8a01f5dc7df2f3b01fa
Jean Ropert <jeanrop@beluga3.int.ets1.calculquebec.ca>
2023-10-24 15:19:07 -0400

    Move files

--- Python/CV-CNN_RCA/beamformer/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 12 15:17:20 2020
-
-@author: essai-2
-"""
-
--- Python/CV-CNN_RCA/simulator/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 12 15:13:58 2020
-
-@author: essai-2
-"""
-
